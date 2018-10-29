10780430c91ed232e6c601a10abd62debb797746
Gustavo <chiappegus@gmail.com>
2018-10-29 00:01:01 -0300

    excelente test mocha :)

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -13,13 +13,34 @@ class Car {
 		return 'vroom';
 	}
 }
+// cmd: npm run test 
+// en el json hay que poner :
+	/* "scripts": {
+	    "test": "mocha"
+	  },*/
+
+let car;
+beforeEach(()=>{
+	//console.log('a');
+	 car = new Car();
+});
 
 describe('Car',()=>
 {
 	it('can park ',()=>{
-		const car = new Car();
+		/*aca va la posta */
+		//const car = new Car();
 		assert.equal(car.park(),'stopped');
 	})
+
+	it('can drive ', ()=>{
+		/*aca va la posta */
+		//const car = new Car();
+		assert.equal(car.drive(),'vroom');
+	})
 })
+
+
+
 
 
