c4b103171c5d2d9d102014226262e7c183629a45
Gustavo <chiappegus@gmail.com>
2018-11-01 01:04:02 -0300

    2  49 Verifying Initial Message

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -8,6 +8,8 @@ const {interface , bytecode} = require('../compiler');
 let accounts;
 let inbox;
 
+const INITIAL_STRING = 'Hi there!'
+
 
 // npm run test
 beforeEach(async()=>{
@@ -30,6 +32,16 @@ describe('Inbox',()=>{
 	it('deploys a contract',()=>{
 		//console.log(inbox);
 		assert.ok(inbox.options.address);
+
+	});
+
+	it('has a default message',async()=>{
+
+		const message = await inbox.methods.message().call();
+		//console.log(message);
+		assert.equal(message,'Hi there!')
+		 
+		
 
 	});
 });
